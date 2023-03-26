d83cb725ecf8ca49b4fd852bd9a9fa3f457e8377
Reona Oshima(totoraj) <totoraj930@yahoo.co.jp>
2023-03-26 22:37:29 +0900

    test

--- src/features/gbs/ads.tsx
+++ src/features/gbs/ads.tsx
@@ -14,103 +14,24 @@ declare global {
   }
 }
 
-/**
- * gbs-footer
- */
-export function UnitGbsFooter() {
-  const [w, setW] = createSignal(Math.min(970, document.body.clientWidth));
-
-  return (
-    <ins
-      class="adsbygoogle"
-      style={{ display: 'inline-block', width: `${w()}px`, height: '100px' }}
-      data-ad-client="ca-pub-5994029821720632"
-      data-ad-slot="8366255152"
-    />
-  );
-}
-
-export function UnitGbs300x250() {
-  return (
-    <ins
-      class="adsbygoogle"
-      style={{ display: 'inline-block', width: '300px', height: '250px' }}
-      data-ad-client="ca-pub-5994029821720632"
-      data-ad-slot="7373836338"
-      // data-ad-client="ca-pub-5994029821720632"
-      // data-ad-slot="5710766688"
-    />
-  );
-}
-
-export function Ads(props: { unit: '300x250' | 'footer' }) {
-  const timer = setTimeout(() => {
-    window.adsbygoogle && window.adsbygoogle.push({});
-  }, 1000);
-
-  onCleanup(() => {
-    clearTimeout(timer);
-  });
-
-  return (
-    <>
-      <Show when={props.unit === '300x250'}>
-        <div class="relative mx-auto h-[250px] w-[300px]">
-          <div
-            class="absolute top-0 left-0"
-            style={{ width: '300px', height: '250px' }}
-          >
-            <UnitGbs300x250 />
-          </div>
-        </div>
-      </Show>
-      <Show when={props.unit === 'footer'}>
-        <div class="h-[100px] max-w-[970px]">
-          <UnitGbsFooter />
-        </div>
-      </Show>
-    </>
-  );
-}
-
 export function AdsColumn() {
   return (
-    <section
-      class={twMerge(
-        clsx(
-          'relative flex h-full shrink-0 flex-col leading-none',
-          'text-gray-700 dark:text-white',
-          'h-[250px] w-[300px]'
-          // 'bg-white, dark:bg-gray-700',
-          // {
-          //   'min-w-[var(--column-size)]': globalSettings.columnSize !== 's',
-          //   'min-w-[300px]': globalSettings.columnSize === 's',
-          //   'h-[calc(50%-3px)]': globalSettings.columnType === '2lines',
-          // }
-        )
-      )}
-    >
-      {/* <div class="absolute top-0 left-0 h-full w-full overflow-hidden text-center"> */}
-      <p class="mb-[5px] mt-[5px] text-center text-[14px]">{text('広告')}</p>
-      <Ads unit="300x250" />
+    <section class="ad-column dark:text-white">
+      <p class="w-full py-[5px] text-center text-[14px]">{text('広告')}</p>
+      <div class="ad-wrap bg-gray-100 dark:bg-gray-800">
+        <ins
+          data-ad-client="ca-pub-5994029821720632"
+          data-ad-slot="5710766688"
+          style={{ display: 'block', width: '300px', height: '250px' }}
+          ref={() => {
+            setTimeout(() => {
+              window.adsbygoogle && window.adsbygoogle.push({});
+            }, 100);
+          }}
+        >
+          {}
+        </ins>
+      </div>
     </section>
-  );
-}
-
-export const [adsFooterH, setAdsFooterH] = createSignal(0);
-
-export function AdsFooter() {
-  return (
-    <footer
-      class={clsx(
-        'h-[100px] max-h-[100px] min-h-[100px] w-full flex-shrink-0 flex-grow-0',
-        'bg-white dark:bg-gray-800',
-        {
-          hidden: isCompact(),
-        }
-      )}
-    >
-      <Ads unit="footer" />
-    </footer>
   );
 }
--- src/features/gbs/index.tsx
+++ src/features/gbs/index.tsx
@@ -23,7 +23,7 @@ import { initAudioContext } from './utils';
 import { twMerge } from 'tailwind-merge';
 import { MsLock, MsLockOpen, MsMenu } from 'solid-material-symbols/rounded/600';
 import { ToastArea } from './Store/toast';
-import { AdsColumn, AdsFooter } from './ads';
+import { AdsColumn } from './ads';
 
 export function Gbs() {
   onMount(async () => {
@@ -74,108 +74,101 @@ export function Gbs() {
   }
 
   return (
-    <div id="gbs-wrap" class={clsx('flex h-full w-full flex-col')}>
+    <div
+      id="gbs-main"
+      class={twMerge(
+        clsx(
+          'flex h-full w-full content-start justify-start gap-[5px] overflow-auto p-[5px]',
+          'flex-1 smh:p-0',
+          'dark:text-red bg-gray-100 text-gray-800 dark:bg-gray-900',
+          'no-pull-to-refresh overscroll-y-none',
+          {
+            'flex-row': globalSettings.columnType === '1line',
+            'flex-nowrap': globalSettings.columnType === '1line',
+            'flex-col': globalSettings.columnType == '2lines',
+            'flex-wrap': globalSettings.columnType == '2lines',
+            'min-h-[340px]': globalSettings.columnType == '2lines',
+            '[--column-size:270px]': globalSettings.columnSize == 's',
+            '[--column-size:320px]': globalSettings.columnSize == 'm',
+            '[--column-size:380px]': globalSettings.columnSize == 'l',
+            'auto-compact': true,
+            'has-focus': hasFocus(),
+            'overflow-hidden': isScreenLock(),
+          }
+        )
+      )}
+    >
+      <ToastArea />
+
+      <Show when={globalSettings.menuPotision === 'left'}>
+        <MenuColumn />
+      </Show>
+
+      <ColumnGroup groupKey={globalSettings.currentGroupKey} />
+
+      <AdsColumn />
+
+      <Show when={globalSettings.menuPotision === 'right'}>
+        <MenuColumn />
+      </Show>
+
       <div
-        id="gbs-main"
-        class={twMerge(
-          clsx(
-            'flex h-full w-full content-start justify-start gap-[5px] overflow-auto p-[5px]',
-            'flex-1 smh:p-0',
-            'dark:text-red bg-gray-100 text-gray-800 dark:bg-gray-900',
-            'no-pull-to-refresh overscroll-y-none',
-            {
-              'flex-row': globalSettings.columnType === '1line',
-              'flex-nowrap': globalSettings.columnType === '1line',
-              'flex-col': globalSettings.columnType == '2lines',
-              'flex-wrap': globalSettings.columnType == '2lines',
-              'min-h-[340px]': globalSettings.columnType == '2lines',
-              '[--column-size:270px]': globalSettings.columnSize == 's',
-              '[--column-size:320px]': globalSettings.columnSize == 'm',
-              '[--column-size:380px]': globalSettings.columnSize == 'l',
-              'auto-compact': true,
-              'has-focus': hasFocus(),
-              'overflow-hidden': isScreenLock(),
-            }
-          )
+        class={clsx(
+          'absolute bottom-0 right-0 gap-[10px] p-[18px]',
+          'pointer-events-none flex flex-col',
+          'z-[40]',
+          {
+            // 'bottom-0': globalSettings.columnType === '2lines' || isCompact(),
+            // 'bottom-[100px]':
+            //   globalSettings.columnType === '1line' && !isCompact(),
+          }
         )}
       >
-        <ToastArea />
-
-        <Show when={globalSettings.menuPotision === 'left'}>
-          <MenuColumn />
-        </Show>
-
-        <ColumnGroup groupKey={globalSettings.currentGroupKey} />
-
-        {/* <Show when={globalSettings.columnType === '2lines'}> */}
-        <AdsColumn />
-        {/* </Show> */}
-
-        <Show when={globalSettings.menuPotision === 'right'}>
-          <MenuColumn />
-        </Show>
-
-        <div
-          class={clsx(
-            'absolute bottom-0 right-0 gap-[10px] p-[18px]',
-            'pointer-events-none flex flex-col',
-            'z-[40]',
-            {
-              // 'bottom-0': globalSettings.columnType === '2lines' || isCompact(),
-              // 'bottom-[100px]':
-              //   globalSettings.columnType === '1line' && !isCompact(),
-            }
+        <button
+          onClick={() => showMenu()}
+          class={twMerge(
+            clsx(
+              'h-[40px] w-[40px] place-content-center',
+              'pointer-events-auto grid rounded-full',
+              'bg-sky-500 text-white',
+              {
+                hidden:
+                  isMenuVisible() ||
+                  isCompact() ||
+                  isScreenLock() ||
+                  !globalSettings.menuButton,
+                'shadow-[0_0_5px_#000]': globalSettings.darkMode,
+                'shadow-[0_0_5px_rgba(0,0,0,0.5)]': !globalSettings.darkMode,
+              }
+            )
           )}
         >
-          <button
-            onClick={() => showMenu()}
-            class={twMerge(
-              clsx(
-                'h-[40px] w-[40px] place-content-center',
-                'pointer-events-auto grid rounded-full',
-                'bg-sky-500 text-white',
-                {
-                  hidden:
-                    isMenuVisible() ||
-                    isCompact() ||
-                    isScreenLock() ||
-                    !globalSettings.menuButton,
-                  'shadow-[0_0_5px_#000]': globalSettings.darkMode,
-                  'shadow-[0_0_5px_rgba(0,0,0,0.5)]': !globalSettings.darkMode,
-                }
-              )
-            )}
+          <MsMenu size={26} />
+        </button>
+        <button
+          onClick={() => setIsScreenLock((prev) => !prev)}
+          class={twMerge(
+            clsx(
+              'h-[40px] w-[40px] place-content-center',
+              'pointer-events-auto grid rounded-full',
+              'bg-sky-500 text-white',
+              {
+                hidden: !globalSettings.lockButton,
+                'shadow-[0_0_5px_#000]': globalSettings.darkMode,
+                'shadow-[0_0_5px_rgba(0,0,0,0.5)]': !globalSettings.darkMode,
+                'bg-red-600': isScreenLock(),
+              }
+            )
+          )}
+        >
+          <Show
+            when={isScreenLock()}
+            fallback={<MsLockOpen size={26} class="-translate-y-[1px]" />}
           >
-            <MsMenu size={26} />
-          </button>
-          <button
-            onClick={() => setIsScreenLock((prev) => !prev)}
-            class={twMerge(
-              clsx(
-                'h-[40px] w-[40px] place-content-center',
-                'pointer-events-auto grid rounded-full',
-                'bg-sky-500 text-white',
-                {
-                  hidden: !globalSettings.lockButton,
-                  'shadow-[0_0_5px_#000]': globalSettings.darkMode,
-                  'shadow-[0_0_5px_rgba(0,0,0,0.5)]': !globalSettings.darkMode,
-                  'bg-red-600': isScreenLock(),
-                }
-              )
-            )}
-          >
-            <Show
-              when={isScreenLock()}
-              fallback={<MsLockOpen size={26} class="-translate-y-[1px]" />}
-            >
-              <MsLock size={26} class="-translate-y-[1px]" />
-            </Show>
-          </button>
-        </div>
+            <MsLock size={26} class="-translate-y-[1px]" />
+          </Show>
+        </button>
       </div>
-      {/* <Show when={globalSettings.columnType === '1line'}>
-        <AdsFooter />
-      </Show> */}
     </div>
   );
 }
