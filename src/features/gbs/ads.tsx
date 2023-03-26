ac81c568dbe8381601fb1127e1d04d292f69ff0b
Reona Oshima(totoraj) <totoraj930@yahoo.co.jp>
2023-03-26 23:48:41 +0900

    demo

--- src/features/gbs/ads.tsx
+++ src/features/gbs/ads.tsx
@@ -7,27 +7,27 @@ declare global {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       push: (v: any) => void;
     };
+    google_ad_client?: string;
+    google_ad_slot?: string;
+    google_ad_width?: number;
+    google_ad_height?: number;
   }
 }
 
 export function AdsColumn() {
+  window.google_ad_client = 'ca-pub-5994029821720632';
+  window.google_ad_slot = '7373836338';
+  window.google_ad_width = 300;
+  window.google_ad_height = 250;
+
   return (
     <section class="ad-column dark:text-white">
       <p class="w-full py-[5px] text-center text-[14px]">{text('広告')}</p>
       <div class="ad-wrap bg-gray-200 dark:bg-gray-800">
-        <ins
-          class="adsbygoogle"
-          data-ad-client="ca-pub-5994029821720632"
-          data-ad-slot="5710766688"
-          style={{ display: 'block', width: '300px', height: '250px' }}
-          ref={() => {
-            setTimeout(() => {
-              window.adsbygoogle && window.adsbygoogle.push({});
-            }, 1);
-          }}
-        >
-          {}
-        </ins>
+        <script
+          type="text/javascript"
+          src="//pagead2.googlesyndication.com/pagead/show_ads.js"
+        />
       </div>
     </section>
   );
